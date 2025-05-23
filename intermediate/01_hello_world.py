85b36d3047279eed3eff73be4701a3742ae0a712
gina <ginaxu1230@gmail.com>
2025-05-24 06:52:32 +1000

    Update 01_unique_numbers.py

--- intermediate/01_hello_world.py
+++ intermediate/01_hello_world.py
@@ -1,1 +1,7 @@
-print("hello, world")
+def unique_elements(lst):
+    return list(set(lst))
+  
+nums = [1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6]
+unique_nums = unique_elements(nums)
+
+print("this is the unique numbers:", unique_nums)
