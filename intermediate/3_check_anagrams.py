2ff6641e30a6e89e2ca4fa2370d5a699c67bb41e
gina <ginaxu1230@gmail.com>
2025-05-24 07:24:48 +1000

    Update 3_check_anagrams.py

--- intermediate/3_check_anagrams.py
+++ intermediate/3_check_anagrams.py
@@ -3,4 +3,8 @@ def anagrams(str1,str2):
 #test
 str1="gina"
 str2="naga"
-print(anagrams(str1,str2))
+
+if anagrams(str1,str2):
+    print(f"{str1} and {str2} are composed of the same letters.")
+else:
+     print(f"{str1} and {str2} are not composed of the same letters.")
