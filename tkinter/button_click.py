6c718607b81ff5096a2d3ebdfe7d8451d58f41db
gina <ginaxu1230@gmail.com>
2025-05-31 19:09:45 +1000

    Update button_click.py

--- tkinter/button_click.py
+++ tkinter/button_click.py
@@ -7,15 +7,15 @@ def button_click(event):
 
 button1= Button(root, text= "one", fg="red")
 button1.bind("<Button-1>", button_click)
-button1.pack(side= LEFT)
+button1.pack(side= LEFT)  # keeping the 3 buttons in the same line
 
 button2= Button(root, text= "two", fg="green")
 button2.bind("<Button-1>", button_click)
-button2.pack(side =LEFT)
+button2.pack(side =LEFT)# keeping the 3 buttons in the same line
 
 button3= Button(root, text= "three", fg="blue")
-button3.bind("<Button-1>", button_click)
-button3.pack(side=LEFT)
+button3.bind("<Button-1>", button_click) 
+button3.pack(side=LEFT)# keeping the 3 buttons in the same line
 
 root.mainloop()
 
