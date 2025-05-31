f01c30f0579e1a18ffc7fa91e62e6b9d4fb85b91
gina886 <ginaxu1230@gmail.com>
2025-05-31 19:08:36 +1000

    button_click

--- tkinter/button_click.py
+++ tkinter/button_click.py
@@ -1,1 +1,22 @@
+from tkinter import *
+ 
+root = Tk ()
+
+def button_click(event):
+    print("button clicked")
+
+button1= Button(root, text= "one", fg="red")
+button1.bind("<Button-1>", button_click)
+button1.pack(side= LEFT)
+
+button2= Button(root, text= "two", fg="green")
+button2.bind("<Button-1>", button_click)
+button2.pack(side =LEFT)
+
+button3= Button(root, text= "three", fg="blue")
+button3.bind("<Button-1>", button_click)
+button3.pack(side=LEFT)
+
+root.mainloop()
+
 
