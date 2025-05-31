88fe4cd746a7d8df7e49e0c4c2ab8d978726c68a
gina886 <ginaxu1230@gmail.com>
2025-05-31 19:37:32 +1000

    frame_mouse_events

--- tkinter/frame_mouse_events.py
+++ tkinter/frame_mouse_events.py
@@ -1,1 +1,22 @@
+from tkinter import *
+root= Tk()
+
+def leftclick(event):
+    print("Left click")
+
+def middleclick(event):
+    print("Middle click")
+
+def rightclick(event):
+    print("Right click")
+
+
+frame = Frame(root, width=200, height=200, bg="pink")
+frame.bind("<Button-1>", leftclick)
+frame.bind("<Button-2>", middleclick)
+frame.bind("<Button-3>", rightclick)
+frame.pack(expand=True, fill=BOTH)
+
+root.mainloop()
+
 
